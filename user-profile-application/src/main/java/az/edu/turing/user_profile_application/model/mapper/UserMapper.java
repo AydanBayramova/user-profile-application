a8ea656649498161ebb84cf225158d14fd796b02
AydanBayramova <bayramoffaaydan1@gmail.com>
2024-08-11 23:45:30 +0400

    use mapstruck implementation and mapper annotation return dto and entity each other

---
+++ user-profile-application/src/main/java/az/edu/turing/user_profile_application/model/mapper/UserMapper.java
@@ -0,0 +1,20 @@
+package az.edu.turing.user_profile_application.model.mapper;
+
+
+
+import az.edu.turing.user_profile_application.domain.entity.UserEntity;
+import az.edu.turing.user_profile_application.model.dto.UserDto;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+
+
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+     UserDto toUserDto(UserEntity user);
+
+     UserEntity toUserEntity(UserDto userDto);
+
+     List<UserDto> toUserDto(List<UserEntity> users);
+}
