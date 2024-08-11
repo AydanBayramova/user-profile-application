044c2d0d977518208ff7b2ec695c0b1a1ee68375
AydanBayramova <bayramoffaaydan1@gmail.com>
2024-08-11 23:56:06 +0400

    application properties

---
+++ user-profile-application/src/main/java/az/edu/turing/user_profile_application/UserProfileApplication.java
@@ -0,0 +1,13 @@
+package az.edu.turing.user_profile_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserProfileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserProfileApplication.class, args);
+	}
+
+}
