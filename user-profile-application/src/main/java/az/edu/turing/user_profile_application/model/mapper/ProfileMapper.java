d0fbfbcde38546eb571942eede5f45c5904bedca
AydanBayramova <bayramoffaaydan1@gmail.com>
2024-08-12 19:29:01 +0400

    add new method ProfileMapper

--- user-profile-application/src/main/java/az/edu/turing/user_profile_application/model/mapper/ProfileMapper.java
+++ user-profile-application/src/main/java/az/edu/turing/user_profile_application/model/mapper/ProfileMapper.java
@@ -4,10 +4,11 @@ package az.edu.turing.user_profile_application.model.mapper;
 import az.edu.turing.user_profile_application.domain.entity.ProfileEntity;
 import az.edu.turing.user_profile_application.model.dto.ProfileDto;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public interface ProfileMapper {
     ProfileEntity  toprofileEntity(ProfileDto profileDto);
     ProfileDto toprofileDto(ProfileEntity profileEntity);
-
+    void updateProfileEntityFromDto(ProfileDto profileDto, @MappingTarget ProfileEntity profileEntity);
 }
