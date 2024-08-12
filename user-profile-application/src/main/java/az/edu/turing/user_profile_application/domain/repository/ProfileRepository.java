dd444ca775ae885957f09017f39d61b673db6152
AydanBayramova <bayramoffaaydan1@gmail.com>
2024-08-12 14:36:11 +0400

    write findbyUser method in Jpa

--- user-profile-application/src/main/java/az/edu/turing/user_profile_application/domain/repository/ProfileRepository.java
+++ user-profile-application/src/main/java/az/edu/turing/user_profile_application/domain/repository/ProfileRepository.java
@@ -1,5 +1,4 @@
-package az.edu.turing.user_profile_application.domain.repository;//package com.boot.usermanagementsystem.domain.repository;
-
+package az.edu.turing.user_profile_application.domain.repository;
 
 import az.edu.turing.user_profile_application.domain.entity.ProfileEntity;
 import org.springframework.data.domain.Page;
@@ -7,8 +6,13 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+
 @Repository
 public interface ProfileRepository extends JpaRepository<ProfileEntity, Long> {
 
-    Page<ProfileEntity> findAll(Pageable pageable);
+    Page<ProfileEntity> findAllByUserId(Long userId, Pageable pageable);
+    Optional<ProfileEntity> findByUserIdAndId(Long userId, Long profileId);
+
 }
